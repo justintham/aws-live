cb3a62bc2c6506163a83368e1afb7a7dbeeb0646
justintham <81354747+justintham@users.noreply.github.com>
2022-04-05 21:20:22 +0800

    Add files via upload

--- JCTY.py
+++ JCTY.py
@@ -67,6 +67,9 @@ def AddEmp():
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
             s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=emp_image_file)
+            s3_object = s3.Object(bucket, emp_image_file_name_in_s3)
+            s3_object.metadata.update({'id':'image/png'})
+            s3_object.copy_from(CopySource={'Bucket':bucket, 'Key':emp_image_file_name_in_s3}, Metadata=s3_object.metadata, MetadataDirective='REPLACE')
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
             s3_location = (bucket_location['LocationConstraint'])
 
@@ -79,9 +82,7 @@ def AddEmp():
                 s3_location,
                 custombucket,
                 emp_image_file_name_in_s3)
-            s3_object = s3.Object(bucket, emp_image_file_name_in_s3)
-            s3_object.metadata.update({'id':'image/png'})
-            s3_object.copy_from(CopySource={'Bucket':bucket, 'Key':emp_image_file_name_in_s3}, Metadata=s3_object.metadata, MetadataDirective='REPLACE')
+
 
         except Exception as e:
             return str(e)
